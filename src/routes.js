aa99beb13ecdef6613b69cbf7d0ca0eb6bc8a87b
yetiwords <adcallaghan159@gmail.com>
2018-10-25 11:39:25 -0700

    account settings styled

--- src/routes.js
+++ src/routes.js
@@ -9,8 +9,9 @@ import ShippingOptions from './Components/Checkout/ShippingOptions';
 import ShippingForm from './Components/Checkout/ShippingForm';
 import NikePlus from './Components/NikePlus/NikePlus';
 import Orders from './Components/Orders/Orders';
-import NikeSettings from './Components/NikeSettings/Settings';
+// import NikeSettings from './Components/AccountSettings/Settings';
 import Wishlist from './Components/Wishlist/Wishlist';
+import Account from './Components/AccountSettings/Account'
 
 export default (
     <Switch>
@@ -18,11 +19,11 @@ export default (
         <Route path='/shippingoptions' component={ShippingOptions}/>
         <Route path='/shippingform' component={ShippingForm}/>
         <Route path="/product/:id" component={DetailedShoe} />
-        <Route path="/cart" component={Cart} />
+       <Route path="/cart" component={Cart} />
         <Route path="/checkout" component={Checkout} />
         <Route path="/nike/nike-plus" component={NikePlus} />
         <Route path="/orders" component={Orders} />
-        <Route path="/settings" component={NikeSettings} />
+        <Route path="/settings" component={Account} />
         <Route path="/wishlist" component={Wishlist} />
         <Route exact path='/' component={Home} />
     </Switch>
