47d9b343dfe48c0630063c44493cf6111c7c5051
Jacob Bralish <JacobTBralish@yahoo.com>
2018-10-26 11:21:09 -0700

    start to sort

---
+++ src/Components/ProductSort/ProductSort.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+
+class ProductSort extends Component {
+    constructor(props) {
+        super(props);
+        this.state = { 
+            input: ''
+         }
+    }
+    render() { 
+        return ( 
+            <div className='left-sort-nav'>
+                <div className='left-sort-nav-header'>
+                    <div className='left-sort-nav-title'>
+                    </div>
+                    <span className='left-sort-nav-header-line'>
+                    </span>
+                </div>
+
+                <div>
+                </div>
+
+                <div>
+                </div>
+
+                <div>
+                </div>
+
+            </div>
+         );
+    }
+}
+ 
+export default ProductSort;
